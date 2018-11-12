10a32c2c99f4d7df4cd22d1fe765d4e7fe5b1b87
M00seMan <andreas.feldt01@gmail.com>
2018-11-12 09:34:03 +0100

    Add files via upload

---
+++ conf.bat
@@ -0,0 +1,3 @@
+@echo off
+powershell -ExecutionPolicy ByPass -noexit "& ""FULL LOCATION OF THE POWERSHELL SCRIPT"""
+exit
---
+++ net.ps1
@@ -0,0 +1,84 @@
+﻿    cls
+    echo "Net tools by M00se"
+    echo ""
+    echo "Type 1 to activate DHCP"
+    echo "Type 2 to decativate DHCP"
+    echo "Type 3 to release/renew DHCP"
+    echo "Type 4 to change static ip"
+    echo "Type 5 to turn off Windows Firewall"
+    echo "Type 6 to turn on Windows Firewall"
+    echo "Type 7 to internet connectivity"
+    $choice = Read-Host "Selection"
+
+
+if($choice -eq 1){
+    cls
+    netsh interface ipv4 show interfaces
+    $NIC = Read-Host 'Select NIC'
+    Set-NetIPInterface -InterfaceIndex $NIC -Dhcp Enabled; ipconfig /renew
+}
+
+
+if($choice -eq 2){
+    cls
+    netsh interface ipv4 show interfaces
+    $NIC = Read-Host 'Select NIC'
+    Set-NetIPInterface -InterfaceIndex $NIC -Dhcp Disabled
+	}
+
+if($choice -eq 3){
+    cls
+    netsh interface ipv4 show interfaces
+    $NIC = Read-Host 'Select NIC'
+    Get-NetAdapter -ifIndex $NIC | Get-NetIPAddress | Remove-NetIPAddress *
+    ipconfig /release
+    ipconfig /renew
+    cls
+    ipconfig
+    echo "Successfully renewed DHCP address"
+}
+
+
+if($choice -eq 4){
+    cls
+    netsh interface ipv4 show interfaces
+    $NIC = Read-Host 'Select Which NIC to configure'
+    $IP = Read-Host 'Enter your desired IP'
+    $NM = Read-Host 'Enter the netmask lenght (eg. 16)'
+    $GW = Read-Host 'Enter your networks gateway'
+
+    route -f
+
+    Set-NetIPInterface -InterfaceIndex $NIC -Dhcp Disabled
+    Set-NetIPInterface -InterfaceIndex $NIC -Dhcp Enabled
+    Set-NetIPInterface -InterfaceIndex $NIC -Dhcp Disabled
+
+	Get-NetAdapter -ifIndex $NIC | Get-NetIPAddress | Remove-NetIPAddress *
+
+	New-NetIPAddress -InterfaceIndex $NIC -IPAddress "$IP" -PrefixLength $NM -DefaultGateway $GW
+	cls
+    sleep(2)
+    ipconfig
+	cls
+	sleep(2)
+	ipconfig
+}
+
+if($choice -eq 5){
+    cls
+    netsh advfirewall set allprofiles state off
+    echo "Windows Firewall sucessfully turned off"
+}
+
+if($choice -eq 6){
+    cls
+    netsh advfirewall set allprofiles state on
+    echo "Windows Firewall sucessfully turned on"
+}
+
+if($choice -eq 7){
+    cls
+    ping 8.8.8.8
+}
+pause
+powershell -ExecutionPolicy ByPass -noexit "& ""FULL LOCATION OF THE POWERSHELL SCRIPT"""
